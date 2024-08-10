cbdaad2313822f9d0b76189c2a4231c536f52645
hms5232 <hms5232@hhming.moe>
2024-08-10 14:35:37 +0800

    feat: hexo new

---
+++ src/hexo.rs
@@ -0,0 +1,25 @@
+use std::process::Command;
+
+/// New a post.
+///
+/// # Arguments
+///
+/// * `title` - The title of the post.
+/// * `scaffold` - The category of the post. It should match the filename in scaffolds folder. Leave it empty string to use default post scaffold.
+///
+/// # Example
+///
+/// ```
+/// let output = hexo::new("hello", "post");
+/// ```
+///
+/// ```
+/// let output = hexo::new("hello", "");
+/// ```
+///
+pub fn new(title: &str, scaffold: &str) -> std::process::Output {
+    Command::new("C:/Program Files/nodejs/hexo.cmd")
+        .args(vec!["new", scaffold, title])
+        .output()
+        .expect("Failed to execute hexo new.")
+}
